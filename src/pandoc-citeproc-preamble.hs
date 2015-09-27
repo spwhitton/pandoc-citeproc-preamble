0ada277b2ed95cf92fde11fa68f93150656c02a7
Sean Whitton <spwhitton@spwhitton.name>
2015-09-26 21:24:59 -0700

    license text

--- src/pandoc-citeproc-preamble.hs
+++ src/pandoc-citeproc-preamble.hs
@@ -1,3 +1,27 @@
+{-
+
+    pandoc-citeproc-preamble --- Insert before the additions made by pandoc-citeproc
+
+    Copyright (C) 2015  Sean Whitton
+
+    This file is part of pandoc-citeproc-preamble.
+
+    pandoc-citeproc-preamble is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    pandoc-citeproc-preamble is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pandoc-citeproc-preamble.  If not, see
+    <http://www.gnu.org/licenses/>.
+
+-}
+
 import           Data.List        (isPrefixOf)
 import           System.Directory (doesFileExist)
 import           System.FilePath  ((<.>), (</>))
